13628e0b6229a970f66afab9c6a0468b35190d4b
wolfieorama <wachirawamunyi@gmail.com>
2015-12-28 11:51:34 +0300

    added update and reject with empty fields

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -19,6 +19,7 @@ class AuthorsController < ApplicationController
 
   # GET /authors/1/edit
   def edit
+    @author.books.build
   end
 
   # POST /authors
@@ -69,6 +70,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params.require(:author).permit(:name)
+      params.require(:author).permit(:name, books_attributes: [:title, :id])
     end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :books
   validates :name, presence: true
+  accepts_nested_attributes_for :books, reject_if: proc { |attributes| attributes['title'].blank? }
 end
